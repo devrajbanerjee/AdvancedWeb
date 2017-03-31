6436a114fad31334c40662cf1a632c8db0dd369f
Devraj Banerjee <devrajbaner@gmail.com>
2017-03-31 08:56:01 -0400

    Advanced Web Tech
    Used canvas, geolocation, video+audio player, and Webstorage

---
+++ FakeTravel/FakeTravel/Views/Locate.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FakeTravel.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Locate : Page
+    {
+        public Locate()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainContent));
+        }
+
+        private void PurchaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(OrderPage));
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var locator = new Geolocator();
+            locator.DesiredAccuracyInMeters = 50;
+            var position = await locator.GetGeopositionAsync();
+            await MyMap.TrySetViewAsync(position.Coordinate.Point, 18D);
+            mySlider.Value = MyMap.ZoomLevel;
+        }
+
+        private  async void setPositionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var myPosition = new Windows.Devices.Geolocation.BasicGeoposition();
+            myPosition.Latitude = 43.6425657;
+            myPosition.Longitude = -79.3892444;
+            
+            var myPoint = new Windows.Devices.Geolocation.Geopoint(myPosition);
+            if(await MyMap.TrySetViewAsync(myPoint, 18D))
+            {
+                instructBox.Text = "Our location is Toronto, near CN Tower";
+            }
+        }
+
+        private void mySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (MyMap != null)
+            {
+                MyMap.ZoomLevel = e.NewValue;
+            }
+        }
+    }
+}
---
+++ FakeTravel/FakeTravel/Views/Video.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.Storage;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FakeTravel.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Video : Page
+    {
+        public Video()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void openFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker fop = new FileOpenPicker();
+            fop.SuggestedStartLocation = PickerLocationId.Desktop;
+            fop.ViewMode = PickerViewMode.Thumbnail;
+            fop.FileTypeFilter.Add(".mp4");
+
+            StorageFile file = await fop.PickSingleFileAsync();
+
+            MediaInstruction.Visibility = Visibility.Collapsed;
+            Link1.Visibility = Visibility.Collapsed;
+            Link2.Visibility = Visibility.Collapsed;
+
+            if(file != null)
+            {
+                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
+                media.SetSource(stream, file.ContentType);
+                media.Play();
+            }
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            if(media.DefaultPlaybackRate != 1)
+            {
+                media.DefaultPlaybackRate = 1.0;
+            }
+            media.Play();
+        }
+
+        private void pause_Click(object sender, RoutedEventArgs e)
+        {
+            media.Pause();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            media.DefaultPlaybackRate = -2.0;
+            media.Play();
+        }
+
+        private void forward_Click(object sender, RoutedEventArgs e)
+        {
+            media.DefaultPlaybackRate = 2.0;
+            media.Play();
+        }
+
+        private void mute_Click(object sender, RoutedEventArgs e)
+        {
+            media.IsMuted = !media.IsMuted;
+        }
+
+        private void volumePlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (media.IsMuted)
+            {
+                media.IsMuted = false;
+            }
+
+            if(media.Volume >= 0)
+            {
+                media.Volume += .1;
+            }
+        }
+
+        private void volumeMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (media.IsMuted)
+            {
+                media.IsMuted = false;
+            }
+
+            if (media.Volume < 0)
+            {
+                media.Volume -= .1;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Inspire));
+        }
+
+        private void PurchaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(OrderPage));
+        }
+
+        private void CanvasButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Canvas));
+        }
+    }
+}
